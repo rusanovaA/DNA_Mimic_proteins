d3c688d2f061cbb694faf02a1c529c68bc957f1e
Anastasiia Rusanova <90505680+rusanovaA@users.noreply.github.com>
2023-03-22 16:00:21 +0400

    Delete Save_prot_count.sh

--- Save_prot_count.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-data_dir=/home/niagara/Storage/D_Sutormin/Metagenomes/N_Rusanova/Prot
-ls >> File_names.txt
-for i in `ls -a $data_dir/ | grep 'fasta' | sed -r "s/(.+).fasta/\1/" | uniq | sort -d`
-do
-cat ${i}.fasta | grep '>' |wc -l | >> Prot_all.txt
-cd $data_dir/clu_rep
-cat ${i}_clu_rep.fasta | grep '>' |wc -l | >> ../Prot_rep.txt
-done
