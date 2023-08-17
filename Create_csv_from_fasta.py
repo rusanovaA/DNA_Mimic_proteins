7605b40b789123337dadfe4f14399b95c7395178
Anastasiia Rusanova <90505680+rusanovaA@users.noreply.github.com>
2023-08-17 16:21:57 -0700

    Add files via upload

---
+++ Create_csv_from_fasta.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Create csv file from fasta")
+parser.add_argument("-f", "--file", help="Fasta with proteins")
+parser.add_argument("-o", "--output", help="csv output")
+args, unknown = parser.parse_known_args()
+
+sequences = {}
+header = ""
+with open(args.file, "r") as file:
+    for line in file:
+        line=line.rstrip()
+        if (line[0] == ">"):
+            if header != "" and data != "":
+                #print(header, data)
+                sequences[header] = data
+            header = line
+            data = ""
+            #sequences[header] = ""
+                    #print(line)
+        else:
+            data += line
+    sequences[header] = data
+
+
+with open(args.output, "w+") as good_out:
+    good_out.write('Protein_name\tSequence\n')
+    for header in sequences.keys():
+        good_out.write("{}\t{}\n".format(header, sequences[header]))
+
+                    
